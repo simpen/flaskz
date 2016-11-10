23d42d50b299892b5357983cb05e1a21b86d76ec
simepn <luoxr258@sina.com>
2016-11-10 17:34:53 +0800

    11a Blog posts

--- config.py
+++ config.py
@@ -11,9 +11,10 @@ class Config:
     MAIL_USE_TLS = True
     MAIL_USE_SSL = False
     MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
-    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    MAIL_PASSWORD = 'twktrnpypylocbca'
+    # MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
     FLASKY_MAIL_SUBJECT_PREFIX = '[Flasky]'
-    FLASKY_MAIL_SENDER = 'Flasky Admin <xxxxxxx@qq.com>'
+    FLASKY_MAIL_SENDER = 'Flasky Admin <471120122@qq.com>'
     FLASKY_ADMIN = os.environ.get('FLASKY_ADMIN')
 
     @staticmethod
